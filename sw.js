45516093c4cc7636703a523e0e8b1f8458633ee7
Ozkan Abdullahoglu <ozkanabdullahoglu@gmail.com>
2018-07-11 23:25:44 +0300

    feat: sw update

--- sw.js
+++ sw.js
@@ -10,37 +10,7 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
             return cache.addAll([
-                "./",
-                "./index.html",
-                "./restaurant.html",
-                "./img/1.jpg",
-                "./img/2.jpg",
-                "./img/3.jpg",
-                "./img/4.jpg",
-                "./img/5.jpg",
-                "./img/6.jpg",
-                "./img/7.jpg",
-                "./img/8.jpg",
-                "./img/9.jpg",
-                "./img/10.jpg",
-                "./img/1_320w.jpg",
-                "./img/2_320w.jpg",
-                "./img/3_320w.jpg",
-                "./img/4_320w.jpg",
-                "./img/5_320w.jpg",
-                "./img/6_320w.jpg",
-                "./img/7_320w.jpg",
-                "./img/8_320w.jpg",
-                "./img/9_320w.jpg",
-                "./img/10_320w.jpg",
-                "./data/restaurants.json",
-                "./manifest.json",
-                "./js/main.js",
-                "./css/styles.css",
-                "./js/dbhelper.js",
-                "./js/restaurant_info.js",
-                "./sw.js"
-               
+                "./"              
             ]);
         })
     );
