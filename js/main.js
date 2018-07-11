2c4c4f61028d5451aabd83fdff22c6f784660e30
Ozkan Abdullahoglu <ozkanabdullahoglu@gmail.com>
2018-07-11 23:42:08 +0300

    feat: service worker installing path is updated

--- js/main.js
+++ js/main.js
@@ -183,7 +183,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
  * Registering serviceWorker
  */
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js').then(function() {
+    navigator.serviceWorker.register('./sw.js').then(function() {
         console.log('Registration worked!');
     }).catch(function() {
         console.log('Registration failed!');
