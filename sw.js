8a8bf00f6d539b96c89fbd225b41671575aa5943
Ozkan Abdullahoglu <ozkanabdullahoglu@gmail.com>
2018-07-11 23:43:41 +0300

    feat: service worker cache path is updated

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function(cache) {
             return cache.addAll([
-                
+                "./",
                 "./index.html",
                 "./restaurant.html",
                 "./img/1.jpg",
